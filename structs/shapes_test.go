01c8914192f1bcbd5058a6abea3ac9191cee227a
Alexander Popov <alex.val.popov@gmail.com>
2018-07-21 17:08:44 +0300

    Extract a helper test function

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,20 +2,22 @@ package main
 
 import "testing"
 
+func assertResult(t *testing.T, got, want float64) {
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(Rectangle{3.0, 4.0})
 	want := 14.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	assertResult(t, got, want)
 }
 
 func TestArea(t *testing.T) {
 	got := Area(Rectangle{3.0, 4.0})
 	want := 12.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	assertResult(t, got, want)
 }

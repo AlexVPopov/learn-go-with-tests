860d2293d62fdc4cf2907131ca07ebb2a38a0615
Alexander Popov <alex.val.popov@gmail.com>
2018-07-21 22:37:26 +0300

    Basic implementation of Search

---
+++ maps/dictionary.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {}
+
+//Search takes a dictionary and a key and returns the value
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
+}

88a4306482902eae4a284dafa7983ee52de2d122
Alexander Popov <alex.val.popov@gmail.com>
2018-07-23 12:10:23 +0300

    Use Greet() for handling http responses

--- di/di.go
+++ di/di.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
-
-func main() {
-	Greet(os.Stdout, "Elodie")
-}
 
 // Greet writes a greeting to the writer
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+// MyGreeterHanlder handles greetings
+func MyGreeterHanlder(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHanlder))
 }

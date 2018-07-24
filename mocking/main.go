67e7cab5e3ca1456a56960a08e2b280e793e8fd8
Alexander Popov <alex.val.popov@gmail.com>
2018-07-24 11:16:25 +0300

    Extract constants

--- mocking/main.go
+++ mocking/main.go
@@ -10,10 +10,13 @@ func main() {
 	Countdown(os.Stdout)
 }
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 // Countdown counts backwords to 0 and prints go
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }

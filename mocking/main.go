a0dc94874b5ca78808774f0afe49e9eb90ce2400
Alexander Popov <alex.val.popov@gmail.com>
2018-07-24 11:38:29 +0300

    Countdown() now takes a sleeper argument

--- mocking/main.go
+++ mocking/main.go
@@ -8,17 +8,30 @@ import (
 )
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
 
 const finalWord = "Go!"
 const countdownStart = 3
 
+// Sleeper adds a delay to execution
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper impelments the sleeper interface
+type DefaultSleeper struct{}
+
+// Sleep delays the exection with 1 second
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 // Countdown counts backwords to 0 and prints go
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }

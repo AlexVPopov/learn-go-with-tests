b7f4e3a0807450c47b0f00497dce3700e7570db0
Alexander Popov <alex.val.popov@gmail.com>
2018-07-26 11:32:33 +0300

    Extract measureReponseTime()

--- select/racer.go
+++ select/racer.go
@@ -9,17 +9,18 @@ func main() {}
 
 // Racer makes requests to two urls and returns the url, from which is first go a response
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureReponseTime(a)
+	bDuration := measureReponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 
 	return b
+}
+
+func measureReponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }

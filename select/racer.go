4e73642aebfecc3eb26b2a1d9896aaa8ec0ba235
Alexander Popov <alex.val.popov@gmail.com>
2018-07-26 12:12:52 +0300

    Make ping calls concurrent

--- select/racer.go
+++ select/racer.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func main() {}
 
 // Racer makes requests to two urls and returns the url, from which is first go a response
 func Racer(a, b string) (winner string) {
-	aDuration := measureReponseTime(a)
-	bDuration := measureReponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureReponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
